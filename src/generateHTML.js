2d79cd010de7b5e8c8047d7dce7d0eaa586082c2
Evan Arbour <evan.arbour@gmail.com>
2021-08-19 08:56:16 -0700

    entered html card format into .js file

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -2,15 +2,65 @@
 // dynamically create manager card based on user info
 const createManager = (employee) => {
     return `
-    
-    `
+    <div class="cell">
+        <div class="card">
+            <div class="employee-name">
+                <h3>${employee.name}</h3>
+            </div>
+            <div class="employee-role">
+                <h5><span class="material-icons">
+                    work
+                    </span>
+                    Manager</h5>
+            </div>
+            <div class="card-section">
+                <p>ID: ${employee.id} </p>
+                <p>Email: ${employee.email} </p>
+                <p>Office Number: ${employee.officeNumber} </p>
+            </div>
+        </div>
+    </div>`
 }
 // dynamically create engineer card 
-const createEngineer = () => {
-
+const createEngineer = (employee) => {
+    return `
+    <div class="cell">
+        <div class="card">
+            <div class="employee-name">
+                <h3>${employee.name}</h3>
+            </div>
+            <div class="employee-role">
+              <h5><span class="material-icons">
+                engineering
+                </span> Engineer</h5>
+            </div>
+            <div class="card-section">
+                <p>ID: ${employee.id} </p>
+                <p>Email: ${employee.email}</p>
+                <p>Github: ${employee.github} </p>
+            </div>
+        </div>
+    </div>`
+    
 }
 // // dynamically create intern card 
-const createIntern = () => {
+const createIntern = (employee) => {
+    return `
+    <div class="cell">
+        <div class="card">
+            <div class="employee-name">
+                <h3${employee.name}</h3>
+            </div>
+            <div class="employee-role">
+                <h5><span class="material-icons">school</span> Intern</h5>
+            </div>
+            <div class="card-section">
+                <p>ID: ${employee.id}</p>
+                <p>Email: ${employee.email}</p>
+                <p>School: ${employee.school}</p>
+            </div>
+        </div>
+    </div>`
 
 }
 
